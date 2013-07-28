a18c2bd518e44c96450a679347cafa47f9887ddf
Andrew Vos <andrew.vos@gmail.com>
2013-07-28 23:56:58 +0100

    output errors

--- pygmentizer.go
+++ pygmentizer.go
@@ -4,12 +4,17 @@ import (
   "runtime"
   "path"
   "io"
+  "io/ioutil"
   "os/exec"
+  "fmt"
 )
 
 func Highlight(language string, code string) string {
   cmd:= exec.Command("/usr/bin/env", "python", pygmentizerPath(), "-l", language, "-f", "html")
   writer, _ := cmd.StdinPipe()
+  errors, _ := cmd.StderrPipe()
+  allErrors,_ := ioutil.ReadAll(errors)
+  fmt.Printf("pygmentizer errors:\n" + string(allErrors) + "\n")
   io.WriteString(writer, code)
   writer.Close()
   output,_ := cmd.Output()
