53803229f65f7933003c21f3fe7f55978b742a01
Andrew Vos <andrew.vos@gmail.com>
2013-07-29 23:20:21 +0100

    install pygments into tmp directory

--- pygmentizer.go
+++ pygmentizer.go
@@ -4,13 +4,21 @@ import (
   "runtime"
   "path"
   "io"
+  "io/ioutil"
+  "os"
   "os/exec"
   "fmt"
   "errors"
+  "archive/zip"
+  "log"
 )
 
 func Highlight(language string, code string) (string, error) {
-  cmd:= exec.Command("/usr/bin/env", "python", pygmentizerPath(), "-l", language, "-f", "html")
+  if _, err := os.Stat(path.Join(tempPath(), "pygments", "pygmentizer.py")); err != nil {
+    installPygments()
+  }
+
+  cmd:= exec.Command("/usr/bin/env", "python", path.Join(tempPath(), "pygments", "pygmentizer.py"), "-l", language, "-f", "html")
   writer, _ := cmd.StdinPipe()
 
   io.WriteString(writer, code)
@@ -24,7 +32,44 @@ func Highlight(language string, code string) (string, error) {
   return string(output), nil
 }
 
-func pygmentizerPath() string {
+func tempPath() string {
   _, filename, _, _ := runtime.Caller(1)
-  return path.Join(path.Dir(filename), "pygmentizer.py")
+  return path.Join(path.Dir(filename), "tmp")
+}
+
+func installPygments() {
+  os.MkdirAll(tempPath(), 0700)
+
+  zipPath := path.Join(tempPath(), "pygments.zip")
+  ioutil.WriteFile(zipPath, PygmentsZip(), 0700)
+
+  r, err := zip.OpenReader(zipPath)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer r.Close()
+
+  for _, file := range r.File {
+    if file.FileInfo().IsDir() == false {
+      rc, err := file.Open()
+      if err != nil {
+        log.Fatal(err)
+      }
+
+      allBytes,err := ioutil.ReadAll(rc)
+      if err != nil {
+        log.Fatal(err)
+      }
+
+      filePath := path.Join(tempPath(), file.Name)
+      os.MkdirAll(path.Dir(filePath), 0700)
+
+      err = ioutil.WriteFile(filePath, allBytes, 0700)
+      if err != nil {
+        log.Fatal(err)
+      }
+
+      rc.Close()
+    }
+  }
 }
